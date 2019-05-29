3b8120fe85606e7fbbb2060d22c5a90acad840e5
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2019-05-30 04:00:10 +0500

    Updated List Item Design
    Customized the apperance of list items.

--- app/src/main/java/net/dyzynz/androidclub/doings/MainActivity.java
+++ app/src/main/java/net/dyzynz/androidclub/doings/MainActivity.java
@@ -37,9 +37,9 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         setSupportActionBar(toolbar);
 
         String[] from = {DBOpenHelper.NOTE_TEXT};
-        int[] to = {android.R.id.text1};
+        int[] to = {R.id.tvNote};
         cursorAdapter = new SimpleCursorAdapter(this,
-                android.R.layout.simple_list_item_1, null, from, to, 0);
+                R.layout.note_list_item, null, from, to, 0);
 
         ListView list = findViewById(android.R.id.list);
         list.setAdapter(cursorAdapter);
